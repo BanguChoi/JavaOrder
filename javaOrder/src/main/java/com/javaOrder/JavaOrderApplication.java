706a650f4ead5d2706e40d15b4df093c86c5b757
bbrotngus <bbrotngus@naver.com>
2024-08-20 22:17:57 +0900

    0820 commit

---
+++ javaOrder/src/main/java/com/javaOrder/JavaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.javaOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOrderApplication.class, args);
+	}
+
+}
