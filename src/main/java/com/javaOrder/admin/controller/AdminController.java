2e09ca370f4d26eef8a2515eb1a22e76a05a7784
BanguChoi <nturtle0723@naver.com>
2024-09-09 09:36:18 +0900

    관리자 페이지 main화면 재설정 (main화면 : 관리자 로그인)

--- src/main/java/com/javaOrder/admin/controller/AdminController.java
+++ src/main/java/com/javaOrder/admin/controller/AdminController.java
@@ -27,16 +27,16 @@ public class AdminController {
 	private final MemberService memberService;
 	private final AdminService adminService;
 	
-	// 대시보드 페이지
+	// 관리자 메인 페이지 (로그인 페이지)
 	@GetMapping("/")
 	public String adminMain(Model model) {
-		return "/admin/dashBoard/dashBoard";
+		return "/admin/signin";
 	}
 	
 	// 로그인 페이지
-	@GetMapping("/signin")
-	public String adminSign() {
-		return "/admin/signin";
+	@GetMapping("/admin/dashboard")
+	public String adminDashboard() {
+		return "/admin/dashboard/dashboard";
 	}
 	
 	// 로그인 처리
@@ -46,7 +46,7 @@ public class AdminController {
 		
 		if(admin != null && admin.getAdminPasswd().equals(adminPassword)) {
 			session.setAttribute("admin", admin);
-			return "redirect:/admin/"; // 성공할 경우 대시보드 페이지로
+			return "redirect:/admin/dashboard"; // 성공할 경우 대시보드 페이지로
 		} else {
 			model.addAttribute("error","아이디 또는 비밀번호가 일치하지 않습니다.");
 			return "admin/signin"; // 실패할 경우 관리자 로그인 페이지
@@ -57,7 +57,7 @@ public class AdminController {
 	@GetMapping("/signout")
 	public String signout(HttpSession session) {
 		session.invalidate();
-		return "redirect:/admin/signin";
+		return "redirect:/admin/";
 	}
 		
 	// 회원 목록 페이지
