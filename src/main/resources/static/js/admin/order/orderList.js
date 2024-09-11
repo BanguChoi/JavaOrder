7c1a6eee192d412c1a74f0f07e8194ba175db0e3
BanguChoi <nturtle0723@naver.com>
2024-09-11 17:32:48 +0900

    주문내역 페이징 오류 잡는중...

--- src/main/resources/static/js/admin/order/orderList.js
+++ src/main/resources/static/js/admin/order/orderList.js
@@ -9,7 +9,6 @@ $(".statusBtn").click(function(){
 // 페이징 처리 
 $(".page-item a").on("click", function(e){
 	e.preventDefault();
-	/*$("#searchForm").find("input[name='pageNum']").val($(this).attr("href"));*/
 	$("#page").val($(this).data("number"));
 	actionProcess("#searchForm", "get", "/admin/orderList");
 });
